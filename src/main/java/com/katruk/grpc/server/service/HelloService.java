1a349a96534a2b9d85bcadd551bf048dbc1f273e
KatrukOV <KatrukOV@gmail.com>
2019-03-30 22:39:32 +0200

    in HelloService.say add somewhen Exception

--- src/main/java/com/katruk/grpc/server/service/HelloService.java
+++ src/main/java/com/katruk/grpc/server/service/HelloService.java
@@ -1,10 +1,11 @@
 package com.katruk.grpc.server.service;
 
 import com.katruk.grpc.server.api.pb.Hello;
+import com.katruk.grpc.server.domain.Random;
+import com.katruk.grpc.server.domain.TimeOut;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import static com.katruk.grpc.server.domain.TimeOut.waiting;
 
 @Service
 @RequiredArgsConstructor
@@ -12,9 +13,13 @@ public class HelloService {
 
     public Hello.HelloResponse say(Hello.HelloRequest request) {
         String name = request.getName();
-        waiting(5, 1700);
+        final int millisecond = new Random(5, 1700).intInRange();
+        new TimeOut(millisecond).waiting();
+        if (millisecond % 3 == 0) {
+            throw new RuntimeException("Something broke");
+        }
         return Hello.HelloResponse.newBuilder()
-                .setGreeting(String.format("Hello, %s !!!", name))
+                .setGreeting(String.format("Hello, %s !!! in %s", name, millisecond))
                 .build();
     }
 
